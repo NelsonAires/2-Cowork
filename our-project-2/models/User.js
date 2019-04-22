8a7c53cc14d1396053259e31889629b20337a480
Hugo <hugomaneira@gmail.com>
2019-04-22 16:56:20 +0100

    login/up

--- our-project-2/models/User.js
+++ our-project-2/models/User.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: String,
-  password: String,
+  email: {
+    type: String, required:true, unique:true},
+  password: {
+    type: String, required:true},
   confirmationCode: {
     type:String,
     unique:true
