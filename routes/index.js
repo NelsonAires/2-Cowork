a43875f9b1565807b682f7a53e30a67439811830
Sofia <ascto1@gmail.com>
2019-04-23 23:02:35 +0100

    updated picture upload/photos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
 const Coworks = require('../models/Coworks')
+// const Picture = require('../models/picture'); 
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -10,6 +12,12 @@ router.get('/', (req, res, next) => {
 router.get('/add-space', (req, res, next) => {
   res.render('add-space');
 });
+
+/* router.get('/', function(req, res, next) {
+  Picture.find((err, pictures) => {
+    res.render('index', {pictures})
+  })
+}); */
 
 router.get('/all-coworks', (req, res, next) => {
   Coworks.find()
@@ -27,27 +35,9 @@ router.get('/cowork-detail/:coworkId', (req, res, next) => {
 }); 
 
 
-/* Book detail page without editor
-router.get('/books/:bookId', (req,res,next) => {
-console.log("The id is", req.params.bookId)
-Book.findById(req.params.bookId)
-.then(bookFromDb => {
-res.render('book-detail', {
-book: bookFromDb
-})
-})
-}) */
-
 router.get('/signed-up', (req, res, next) => {
   res.render('signed-up');
 }); 
-
-
-
-
-
-
-
 
 
 
